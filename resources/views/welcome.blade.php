374dd028acf169aa2462a780d325a1d17e58506c
Guilherme Ferreti <guiferretis@gmail.com>
2025-04-28 17:37:16 -0300

    Formatted code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+        />
 
         <title>Laravel</title>
 
@@ -10,9 +13,8 @@
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])           
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
     </head>
-    <body>
-    </body>
+    <body></body>
 </html>

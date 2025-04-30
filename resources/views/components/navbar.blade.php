0dd9169ff3370646e8b316da2070f9b8848204f7
Guilherme Ferreti <guiferretis@gmail.com>
2025-04-30 09:31:53 -0300

    Added desktop navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,54 @@
 <nav class="navbar">
-    <ul>
-        <li>Home</li>
-        <li>Courses</li>
-        <li>About Us</li>
-        <li>Pricing</li>
-        <li>Contact</li>
+    <ul class="navbar__links">
+        <li class="navbar__logo">
+            <a href="{{ route('home') }}">
+                <x-icons.app-logo />
+            </a>
+        </li>
+        <li class="navbar__link">
+            <a
+                href="{{ route('home') }}"
+                title="Home"
+            >
+                Home
+            </a>
+        </li>
+        <li class="navbar__link">
+            <a
+                href="#"
+                title="Courses"
+            >
+                Courses
+            </a>
+        </li>
+        <li class="navbar__link">
+            <a
+                href="#"
+                title="About Us"
+            >
+                About Us
+            </a>
+        </li>
+        <li class="navbar__link">
+            <a
+                href="#"
+                title="Pricing"
+            >
+                Pricing
+            </a>
+        </li>
+        <li class="navbar__link">
+            <a
+                href="#"
+                title="Contact"
+            >
+                Contact
+            </a>
+        </li>
     </ul>
+
+    <div class="navbar__actions">
+        <button>Sign Up</button>
+        <button>Login</button>
+    </div>
 </nav>

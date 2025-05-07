940de2835000ebf3f8aba0900e01a36fe9f89992
Guilherme Ferreti <guiferretis@gmail.com>
2025-05-07 09:56:52 -0300

    Moved hero section to component

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,32 @@
+<section class="hero">
+    <div class="hero__header">
+        <x-icons.abstract-line class="hero__header-abstract-line" />
+        <div class="hero__header-icon-wrapper">
+            <x-icons.bolt />
+        </div>
+        <h1 class="hero__header-title">
+            <span>Unlock</span>
+            Your Creative Potential
+        </h1>
+    </div>
+
+    <h2 class="hero__title">with Online Design and Development Courses.</h2>
+    <p class="hero__text">Learn from Industry Experts and Enhance Your Skills.</p>
+
+    <div class="hero__buttons-wrapper">
+        <x-link
+            :href="route('home')"
+            appearance="primary"
+            title="Explore Courses"
+        >
+            Explore Courses
+        </x-link>
+        <x-link
+            :href="route('home')"
+            appearance="secondary"
+            title="View Pricing"
+        >
+            View Pricing
+        </x-link>
+    </div>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,5 @@
 <x-layouts.app>
     <x-slot:title>Power Your Potential</x-slot>
 
-    <sction class="hero">
-        <div class="hero__header">
-            <x-icons.abstract-line class="hero__header-abstract-line" />
-            <div class="hero__header-icon-wrapper">
-                <x-icons.bolt />
-            </div>
-            <h1 class="hero__header-title">
-                <span>Unlock</span>
-                Your Creative Potential
-            </h1>
-        </div>
-
-        <h2 class="hero__title">with Online Design and Development Courses.</h2>
-        <p class="hero__text">Learn from Industry Experts and Enhance Your Skills.</p>
-
-        <div class="hero__buttons-wrapper">
-            <x-link
-                :href="route('home')"
-                appearance="primary"
-                title="Explore Courses"
-            >
-                Explore Courses
-            </x-link>
-            <x-link
-                :href="route('home')"
-                appearance="secondary"
-                title="View Pricing"
-            >
-                View Pricing
-            </x-link>
-        </div>
-    </sction>
+    <x-hero />
 </x-layouts.app>

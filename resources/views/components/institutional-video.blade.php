717e0ff4babb1936d4dac9e5d41c8e9ef642a470
Guilherme Ferreti <guiferretis@gmail.com>
2025-05-20 10:28:53 -0300

    Added video player component

--- resources/views/components/institutional-video.blade.php
+++ resources/views/components/institutional-video.blade.php
@@ -1,18 +1,3 @@
 <div class="institutional-video">
-    <div class="institutional-video__wrapper">
-        <video
-            class="video-js vjs-fluid"
-            id="institutional-video__player"
-            controls
-            loop
-            preload="auto"
-            poster="{{ asset('images/team-members.webp') }}"
-            data-setup="{}"
-        >
-            <source
-                src="{{ asset('videos/institutional-video.mp4') }}"
-                type="video/mp4"
-            />
-        </video>
-    </div>
+    <x-video-player :src="asset('videos/institutional-video.mp4')" :poster="asset('images/team-members.webp')" />
 </div>
---
+++ resources/views/components/video-player.blade.php
@@ -0,0 +1,23 @@
+@props([
+    'src',
+    'poster',
+    'id' => 'video-player-' . uniqid()
+])
+
+<div class="video-player">
+    <video
+        class="video-player__player | video-js vjs-16-9"
+        id="{{ $id }}"
+        controls
+        loop
+        preload="auto"
+        poster="{{ $poster }}"
+        data-setup=""
+        >
+            <source
+                src="{{ $src }}"
+                type="video/mp4"
+            />
+        </video>
+    </div>
+</div>

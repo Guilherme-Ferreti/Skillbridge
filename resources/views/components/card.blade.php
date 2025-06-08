65fe47276308f707214d9a30f65e3c085a3e73c1
Guilherme Ferreti <guiferretis@gmail.com>
2025-06-08 13:44:54 -0300

    feat: add slots to card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,15 @@
-<div class="card">
-    {{ $slot }}
-</div>
+@props([
+    'element' => 'div',
+])
+
+<{{ $element }} {{ $attributes->merge(['class' => 'card']) }}>
+    <div {{ $heading->attributes->class(['card__heading']) }}>
+        {{ $heading }}
+    </div>
+    <div class="card__body">
+        {{ $slot }}
+    </div>
+    <div {{ $footer->attributes->class(['card__footer']) }}>
+        {{ $footer }}
+    </div>
+</{{ $element }}>

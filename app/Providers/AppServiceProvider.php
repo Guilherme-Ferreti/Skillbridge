4b8f6428c6705093eea19e00f63d601411539539
Guilherme Ferreti <guiferretis@gmail.com>
2025-06-13 16:05:51 -0300

    feat: add better Laravel defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +21,18 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Vite::useAggressivePrefetching();
+
+        URL::forceHttps();
+
+        Date::use(CarbonImmutable::class);
+
+        Password::defaults(fn (): ?Password => app()->isProduction() ? Password::min(12)->max(255)->uncompromised() : null);
+
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
     }
 
     /**

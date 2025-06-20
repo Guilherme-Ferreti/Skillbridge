a4970cc83472a1c9fb232e35159519493db85f24
Guilherme Ferreti <guiferretis@gmail.com>
2025-06-20 11:52:22 -0300

    feat: improve hero title SEO

--- resources/views/layouts/home/hero.blade.php
+++ resources/views/layouts/home/hero.blade.php
@@ -1,16 +1,16 @@
 <section class="hero">
-    <div class="hero__header">
-        <x-icons.abstract-line class="hero__header-abstract-line" />
-        <div class="hero__header-icon-wrapper">
-            <x-icons.bolt />
+    <h1 class="hero__heading">
+        <div class="hero__first-line-title-wrapper">
+            <x-icons.abstract-line class="hero__abstract-line" />
+            <div class="hero__icon-wrapper"><x-icons.bolt /></div>
+            <span class="hero__first-line-title">
+                <span>Unlock</span>
+                Your Creative Potential
+            </span>
         </div>
-        <h1 class="hero__header-title">
-            <span>Unlock</span>
-            Your Creative Potential
-        </h1>
-    </div>
+        <span class="hero__second-line-title">with Online Design and Development Courses</span>
+    </h1>
 
-    <h2 class="hero__title">with Online Design and Development Courses.</h2>
     <p class="hero__text">Learn from Industry Experts and Enhance Your Skills.</p>
 
     <div class="hero__buttons-wrapper">

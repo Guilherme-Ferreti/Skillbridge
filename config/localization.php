e215395c340b4c0595e252e1c9fbf22ae4ca37e2
Guilherme Ferreti <guiferretis@gmail.com>
2025-07-21 16:45:22 -0300

    feat: define localized routes

--- config/localization.php
+++ config/localization.php
@@ -147,20 +147,20 @@ return [
                 // This option contains settings for routes without the prefix of the localization code.
 
                 'default' => [
-                    LaravelLang\Routes\Middlewares\LocalizationByCookie::class,
-                    LaravelLang\Routes\Middlewares\LocalizationByHeader::class,
-                    LaravelLang\Routes\Middlewares\LocalizationBySession::class,
-                    LaravelLang\Routes\Middlewares\LocalizationByModel::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationByCookie::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationByHeader::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationBySession::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationByModel::class,
                 ],
 
                 // This option contains settings for routes with the prefix of the localization code.
 
                 'prefix' => [
                     LaravelLang\Routes\Middlewares\LocalizationByParameterPrefix::class,
-                    LaravelLang\Routes\Middlewares\LocalizationByCookie::class,
-                    LaravelLang\Routes\Middlewares\LocalizationByHeader::class,
-                    LaravelLang\Routes\Middlewares\LocalizationBySession::class,
-                    LaravelLang\Routes\Middlewares\LocalizationByModel::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationByCookie::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationByHeader::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationBySession::class,
+                    // LaravelLang\Routes\Middlewares\LocalizationByModel::class,
                 ],
             ],
         ],
--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)->name('home');
-Route::get('/courses', HomeController::class)->name('courses');
-Route::get('/about-us', HomeController::class)->name('about-us');
-Route::get('/pricing', HomeController::class)->name('pricing');
-Route::get('/contact', HomeController::class)->name('contact');
-Route::get('/sign-up', HomeController::class)->name('sign-up');
-Route::get('/login', HomeController::class)->name('login');
+Route::localizedGroup(function () {
+    Route::get('/', HomeController::class)->name('home');
+    Route::get('/courses', HomeController::class)->name('courses');
+    Route::get('/about-us', HomeController::class)->name('about-us');
+    Route::get('/pricing', HomeController::class)->name('pricing');
+    Route::get('/contact', HomeController::class)->name('contact');
+    Route::get('/sign-up', HomeController::class)->name('sign-up');
+    Route::get('/login', HomeController::class)->name('login');
+});

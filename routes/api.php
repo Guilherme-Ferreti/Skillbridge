2a5942717dc9aca554738c006cc41935bd2c5479
Guilherme Ferreti <guiferretis@gmail.com>
2025-07-31 15:16:12 -0300

    feat: add FAQ apit endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Api\FaqController;
 use App\Http\Controllers\Api\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', HomeController::class)->name('api.home.show');
+Route::get('/home', HomeController::class)->name('api.home');
+Route::get('/faq', FaqController::class)->name('api.faq');

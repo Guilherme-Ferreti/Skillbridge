1b4220073b6f9e519523e4acb0f75573b7bcd00c
Guilherme Ferreti <guiferretis@gmail.com>
2025-09-29 14:01:56 -0300

    fix: courses count in courses page description

--- resources/views/pages/courses/index.blade.php
+++ resources/views/pages/courses/index.blade.php
@@ -6,7 +6,7 @@
 
     <x-page.content-header
         :title="__('Online Courses on Design and Development')"
-        :description="__('Welcome to our online course page, where you can enhance your skills in design and development. Choose from our carefully curated selection of 10 courses designed to provide you with comprehensive knowledge and practical experience. Explore the courses below and find the perfect fit for your learning journey.')"
+        :description="__('Welcome to our online course page, where you can enhance your skills in design and development. Choose from our carefully curated selection of :courses_count courses designed to provide you with comprehensive knowledge and practical experience. Explore the courses below and find the perfect fit for your learning journey.', ['courses_count' => $courses->count()])"
     />
 
     @include('layouts.courses.index.courses-list')

84985c1caeb1c222bf09fdf4c768d445b037ed2e
Guilherme Ferreti <guiferretis@gmail.com>
2025-09-30 19:53:34 -0300

    feat: add module lessons to course form

--- app/Filament/Resources/Courses/RelationManagers/ModulesRelationManager.php
+++ app/Filament/Resources/Courses/RelationManagers/ModulesRelationManager.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Filament\Resources\Courses\RelationManagers;
 
 use App\Enums\Locale;
+use App\Models\Lesson;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Components\Tabs;
@@ -37,6 +39,7 @@ final class ModulesRelationManager extends RelationManager
                                 self::nameInput(Locale::BRAZILIAN_PORTUGUESE),
                             ]),
                     ]),
+                self::lessonsInput(),
             ]);
     }
 
@@ -75,11 +78,55 @@ final class ModulesRelationManager extends RelationManager
             ]);
     }
 
+    private static function lessonNameInput(Locale $locale): TextInput
+    {
+        return TextInput::make("name.$locale->value")
+            ->label('Name')
+            ->required()
+            ->maxLength(255);
+    }
+
     private function nameInput(Locale $locale): TextInput
     {
         return TextInput::make("name.$locale->value")
             ->label('Name')
             ->required()
             ->maxLength(255);
+    }
+
+    private function lessonsInput(): Repeater
+    {
+        return Repeater::make('lessons')
+            ->relationship('lessons')
+            ->itemLabel(fn (array $state) => $state['name'][Locale::ENGLISH->value] . ' - ' . $state['duration'])
+            ->collapsible()
+            ->collapsed()
+            ->mutateRelationshipDataBeforeFillUsing(function (array $data) {
+                $record = Lesson::find($data['id']);
+
+                return [
+                    ...$data,
+                    'name' => [
+                        Locale::ENGLISH->value              => $record->getTranslation('name', Locale::ENGLISH->value),
+                        Locale::BRAZILIAN_PORTUGUESE->value => $record->getTranslation('name', Locale::BRAZILIAN_PORTUGUESE->value),
+                    ],
+                ];
+            })
+            ->schema([
+                Tabs::make()
+                    ->tabs([
+                        Tab::make(Locale::ENGLISH->label())
+                            ->schema([
+                                self::lessonNameInput(Locale::ENGLISH),
+                            ]),
+                        Tab::make(Locale::BRAZILIAN_PORTUGUESE->label())
+                            ->schema([
+                                self::lessonNameInput(Locale::BRAZILIAN_PORTUGUESE),
+                            ]),
+                    ]),
+                TextInput::make('duration')
+                    ->type('time')
+                    ->required(),
+            ]);
     }
 }

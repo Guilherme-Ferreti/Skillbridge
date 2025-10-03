5cf1aa2ca48dca8864773f39cc853e008ebd3806
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-02 21:36:17 -0300

    feat: add author picture to testimonials table

--- app/Filament/Resources/Testimonials/Tables/TestimonialsTable.php
+++ app/Filament/Resources/Testimonials/Tables/TestimonialsTable.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Testimonials\Tables;
 
+use App\Models\Testimonial;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -17,8 +19,11 @@ final class TestimonialsTable
         return $table
             ->columns([
                 TextColumn::make('author_name')
-                    ->sortable()
-                    ->searchable(),
+                ->sortable()
+                ->searchable(),
+                ImageColumn::make('author_picture')
+                    ->defaultImageUrl(fn (Testimonial $testimonial): string => $testimonial->authorPicture())
+                    ->circular(),
                 TextColumn::make('created_at')
                     ->dateTime('M, d Y - H:i')
                     ->sortable(),
